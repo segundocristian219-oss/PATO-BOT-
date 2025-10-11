b8dbe7860e83059e30e07a17d83e70f5bccf59e9
segundocristian219-oss <segundocristian219@gmail.com>
2025-10-11 00:07:59 -0600

    Actualizar config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ import chalk from 'chalk'
 import { fileURLToPath } from 'url'
 
 global.owner = [
-   ['38354561278087', 'angel 63', true],
+   ['38779947585767', 'Duque', true],
     ['59627769213003', 'cristian', true]
 ]
 
